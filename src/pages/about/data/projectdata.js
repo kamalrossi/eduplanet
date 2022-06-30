8b0ed8889c9c12f9d6e7db6bb9c182e90c0990bb
kamalrossi <rossikamal@gmail.com>
2022-06-30 21:36:49 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,47 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "EduPlanet";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "EduPlanet is a responsive admin dashboard to manage regular academic activiies in a school/college environment. Major modules include student, teacher, attendance, tuition, courses, reporting etc."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "ChartJS",
+    tag3: "D3",
+    tag4: "EdTech"
+  }
+];
+const projecttime = [
+  {
+    title: "UX/UI  Design",
+    from: "Apr 22",
+    to: "Jun 22"
+  },
+  {
+    title: "Responsive Web Development",
+    from: "Apr 22",
+    to: "Jun 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UX Design",
+    desc: "User Experience analysis in education platform"
+  },
+  {
+    title: "Responsive Web Development",
+    desc: " Development of responsive web application as admin dashboard"
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
